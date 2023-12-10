fe10309c20b2cf6e07e3cd0f58afc963f8bba39e
TheEffectik <andruxa-mix112@yandex.ru>
2023-12-10 23:03:06 +0300

    add scale

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     var scale = 1;
     var minScale = 1;
-    var maxScale = 4;
+    var maxScale = 10;
     var zoomSpeed = 0.1;
     var transform = { x: 0, y: 0 };
     var startMousePosition = { x: 0, y: 0 };
@@ -9,34 +9,60 @@ document.addEventListener('DOMContentLoaded', () => {
     
     
     document.addEventListener('dragover', (event) => {
-            // Рассчитываем смещение относительно начальной позиции мыши
-            let dx = event.clientX - startMousePosition.x;
-            let dy = event.clientY - startMousePosition.y;
-    
-            // Обновляем координаты transform
-            transform.x += dx;
-            transform.y += dy;
-    
-            updateTransform(); // Применяем новые координаты
-    
-            // Обновляем начальную позицию мыши для следующего перемещения
-            startMousePosition.x = event.clientX;
-            startMousePosition.y = event.clientY;
+        event.preventDefault();
+        // mapImage.style.cursor = 'grabbing';
+        // document.body.style.cursor = 'none';
+        // Рассчитываем смещение относительно начальной позиции мыши
+        let dx = event.clientX - startMousePosition.x;
+        let dy = event.clientY - startMousePosition.y;
+
+        // Обновляем координаты transform
+        transform.x += dx;
+        transform.y += dy;
+
+        updateTransform(); // Применяем новые координаты
+
+        // Обновляем начальную позицию мыши для следующего перемещения
+        startMousePosition.x = event.clientX;
+        startMousePosition.y = event.clientY;           
     });
     document.addEventListener('dragstart', (event) => {
-        if (event.button === 0) {
-            startMousePosition.x = event.clientX; // Запоминаем начальную позицию мыши
-            startMousePosition.y = event.clientY;
-        }
+        startMousePosition.x = event.clientX; // Запоминаем начальную позицию мыши
+        startMousePosition.y = event.clientY;
+        // mapImage.style.cursor = 'grabbing';
     });
     
+    document.addEventListener('dragleave', (event) => {
+        startMousePosition.x = event.clientX; // Запоминаем начальную позицию мыши
+        startMousePosition.y = event.clientY;
+        // mapImage.style.cursor = 'default';      
+    });
+
+    function scaleAt(at, amount) {
+        scale *= amount;
+        pos.x = at.x - (at.x - pos.x) * amount;
+        pos.y = at.y - (at.y - pos.y) * amount;
+    }
+
     mapImage.addEventListener('wheel', (event) => {
         event.preventDefault();
+    
+        // Получение текущего положения курсора относительно изображения
+        const rect = mapImage.getBoundingClientRect();
+        const cursorX = event.clientX - transform.x;
+        const cursorY = event.clientY - transform.y;
+    
+        // Рассчет нового масштаба
         var delta = event.deltaY > 0 ? -zoomSpeed : zoomSpeed;
         var newScale = Math.max(minScale, Math.min(scale + delta, maxScale));
-
+    
         if (newScale !== scale) {
+            // Рассчет смещения
+            transform.x *= (newScale / scale)
+            transform.y *= (newScale / scale)
+            
             scale = newScale;
+    
             updateTransform();
         }
     });
@@ -45,74 +71,3 @@ document.addEventListener('DOMContentLoaded', () => {
         mapImage.style.transform = `translate(${transform.x}px, ${transform.y}px) scale(${scale})`;
     }
 });
-
-
-// document.addEventListener('DOMContentLoaded', () => {
-   
-// });
-
-
-// $(document).ready(function() {
-//     var scale = 1;
-//     var zoomSpeed = 0.1;
-//     var isDragging = false;
-//     var previousMousePosition = { x: 0, y: 0 };
-//     var transform = { x: 0, y: 0 };
-
-//     $('.map-image').on('wheel', function(event) {
-//         event.preventDefault();
-        
-//         var offsetX = event.offsetX;
-//         var offsetY = event.offsetY;
-//         var imageWidth = $(this).width();
-//         var imageHeight = $(this).height();
-
-//         // Расчет процентов положения курсора относительно контейнера
-//         var originX = (offsetX / imageWidth) * 100;
-//         var originY = (offsetY / imageHeight) * 100;
-
-//         // Изменение масштаба
-//         scale += event.originalEvent.deltaY * -zoomSpeed;
-//         scale = Math.min(Math.max(1, scale), 4); // Ограничение масштаба между 1 и 4
-
-//         // Применение масштаба и изменение центра трансформации
-//         $(this).find('.map-image').css({
-//             'transform-origin': `${originX}% ${originY}%`,
-//             'transform': 'scale(' + scale + ')'
-//         });
-//     });
-
-
-//     // $('.map-image').on('mousedown', function(e) {
-//     //     if (e.button === 0) { // Проверка на левую кнопку мыши
-//     //         isDragging = true;
-//     //         previousMousePosition.x = e.clientX;
-//     //         previousMousePosition.y = e.clientY;
-//     //         $('.map-image').css('cursor', 'grabbing');
-//     //     }
-//     // });
-
-//     // $(document).on('mousemove', function(e) {
-//     //     if (isDragging) {
-//     //         var dx = e.clientX - previousMousePosition.x;
-//     //         var dy = e.clientY - previousMousePosition.y;
-//     //         transform.x += dx;
-//     //         transform.y += dy;
-//     //         updateTransform();
-
-//     //         previousMousePosition.x = e.clientX;
-//     //         previousMousePosition.y = e.clientY;
-//     //     }
-//     // });
-
-//     // $(document).on('mouseup', function(e) {
-//     //     if (e.button === 0) {
-//     //         isDragging = false;
-//     //         $('.map-image').css('cursor', 'grab');
-//     //     }
-//     // });
-
-//     function updateTransform() {
-//         $('.map-image').css('transform', `translate(${transform.x}px, ${transform.y}px) scale(${scale})`);
-//     }
-// });
